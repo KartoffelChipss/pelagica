5730ec54d026eb4811afa6c5fe686413d69b12ea
KartoffelChips <104089082+KartoffelChipss@users.noreply.github.com>
2025-12-31 14:25:53 +0100

    Made posters in rows larger on larger displays

--- src/pages/Home/ItemsRow.tsx
+++ src/pages/Home/ItemsRow.tsx
@@ -45,12 +45,12 @@ const ItemsRow = ({ title, allLink, items }: ItemsRowProps) => {
                     recentItems
                         ? recentItems.map((item) => (
                               <Link to={`/item/${item.Id}`} key={item.Id}>
-                                  <div className="relative w-36 h-54 overflow-hidden rounded-md group">
+                                  <div className="relative w-36 h-54 lg:w-44 lg:h-64 2xl:w-52 2xl:h-80 overflow-hidden rounded-md group">
                                       <img
                                           key={item.Id}
                                           src={getPosterUrl(item.Id ?? '')}
                                           alt={item.Name || 'No Title'}
-                                          className="min-w-36 w-36 min-h-54 h-54 object-cover rounded-md group-hover:opacity-75 transition-all group-hover:scale-105 z-10"
+                                          className="min-w-36 lg:min-w-44 2xl:min-w-52 w-36 lg:w-44 2xl:w-52 min-h-54 lg:min-h-64 2xl:min-h-80 h-54 lg:h-64 2xl:h-80 object-cover rounded-md group-hover:opacity-75 transition-all group-hover:scale-105 z-10"
                                           loading="lazy"
                                       />
                                       <Button
@@ -60,9 +60,9 @@ const ItemsRow = ({ title, allLink, items }: ItemsRowProps) => {
                                       >
                                           <Play size={16} />
                                       </Button>
-                                      <Skeleton className="absolute bottom-0 left-0 right-0 h-54 -z-1" />
+                                      <Skeleton className="absolute bottom-0 left-0 right-0 h-54 lg:h-64 2xl:h-80 -z-1" />
                                   </div>
-                                  <p className="mt-2 text-sm line-clamp-1 text-ellipsis break-all max-w-36">
+                                  <p className="mt-2 text-sm line-clamp-1 text-ellipsis break-all max-w-36 lg:max-w-44 2xl:max-w-52">
                                       {item.Name}
                                   </p>
                                   <p className="text-xs text-muted-foreground">
