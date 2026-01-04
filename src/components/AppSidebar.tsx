bf35e5f7698524410c699d193db6a3fd22218a35
KartoffelChips <104089082+KartoffelChipss@users.noreply.github.com>
2026-01-04 12:02:10 +0100

    Added logo

--- src/components/AppSidebar.tsx
+++ src/components/AppSidebar.tsx
@@ -14,12 +14,13 @@ import {
 import { Home, Library, Search } from 'lucide-react';
 import { Link } from 'react-router';
 import { NavUser } from './NavUser';
-import { Avatar, AvatarFallback } from './ui/avatar';
+import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar';
 import { useTranslation } from 'react-i18next';
 import { useSearch } from '@/context/SearchContext';
 import { useUserViews } from '@/hooks/api/useMediaFolders';
 import JellyfinLibraryIcon from './JellyfinLibraryIcon';
 import { getServerUrl } from '@/utils/localstorageCredentials';
+import { useTheme } from './theme-provider';
 
 function serverUrlToDomain(url: string) {
     try {
@@ -36,6 +37,13 @@ const AppSidebar = () => {
     const { data: views } = useUserViews();
     const serverUrl = getServerUrl();
     const serverDomain = serverUrl ? serverUrlToDomain(serverUrl) : null;
+    const { theme } = useTheme();
+    const effectiveTheme =
+        theme === 'system'
+            ? window.matchMedia('(prefers-color-scheme: dark)').matches
+                ? 'dark'
+                : 'light'
+            : theme;
 
     return (
         <Sidebar variant="floating" collapsible="icon">
@@ -45,7 +53,11 @@ const AppSidebar = () => {
                         size="lg"
                         className="cursor-default hover:bg-transparent active:bg-transparent"
                     >
-                        <Avatar className="h-8 w-8 rounded-lg">
+                        <Avatar className="h-8 w-8 p-1 rounded-lg">
+                            <AvatarImage
+                                src={effectiveTheme === 'dark' ? '/logo.svg' : '/logo-dark.svg'}
+                                alt={'Pelagica logo'}
+                            />
                             <AvatarFallback className="rounded-lg">{'PE'}</AvatarFallback>
                         </Avatar>
                         <div className="grid flex-1 text-left text-sm leading-tight">
