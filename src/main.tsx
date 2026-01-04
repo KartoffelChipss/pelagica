aa97951d2a1ab1652d0a9b3fbc990901d3d3e561
KartoffelChips <104089082+KartoffelChipss@users.noreply.github.com>
2026-01-04 10:45:13 +0100

    Added 404 page

--- src/main.tsx
+++ src/main.tsx
@@ -12,6 +12,7 @@ import { SearchProvider } from './context/SearchProvider.tsx';
 import { SearchCommand } from './components/SearchCommand.tsx';
 import { KeyboardShortcuts } from './components/KeyboardShortcuts.tsx';
 import ItemPage from './pages/Item/ItemPage.tsx';
+import NotFoundPage from './pages/NotFound/NotFoundPage.tsx';
 
 const queryClient = new QueryClient();
 
@@ -27,6 +28,7 @@ createRoot(document.getElementById('root')!).render(
                         <Route path="/library" element={<LibraryPage />} />
                         <Route path="/item/:itemId" element={<ItemPage />} />
                         <Route path="/login" element={<LoginPage />} />
+                        <Route path="*" element={<NotFoundPage />} />
                     </Routes>
                 </BrowserRouter>
             </SearchProvider>
