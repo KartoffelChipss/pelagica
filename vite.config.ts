172c78944260aa9099b6b7f4fe95fdf2d31955fe
KartoffelChips <104089082+KartoffelChipss@users.noreply.github.com>
2026-01-07 11:50:52 +0100

    Moved videjs into own chunk

--- vite.config.ts
+++ vite.config.ts
@@ -15,4 +15,13 @@ export default defineConfig({
         port: 3000,
         allowedHosts: ['mbjan.local'],
     },
+    build: {
+        rollupOptions: {
+            output: {
+                manualChunks: {
+                    videojs: ['video.js'],
+                },
+            },
+        },
+    },
 });
